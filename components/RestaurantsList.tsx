42ea63857c577b7dc4e9e824ba47bdf3b8b4a25b
Eloi Pampliega <eloipampliega@gmail.com>
2024-03-01 15:35:44 +0100

    improved details button.

--- components/RestaurantsList.tsx
+++ components/RestaurantsList.tsx
@@ -30,37 +30,43 @@ export default async function RestaurantsList() {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
       {restaurants.map((r: IRestaurant) => (
-        <Link key={r._id} href={`/restaurant/${r._id}`}>
-          <div
-            className="border border-gray-200 rounded-lg overflow-hidden bg-white shadow-md transition duration-300 hover:shadow-xl"
-          >
-            <div className="relative w-full h-64">
-              <Image
-                src={r.image ?? "/restaurant-default-image.jpg"}
-                alt={r.name + " Restaurant"}
-                width={1000}
-                height={1000}
-                className="w-fit mx-auto h-[-webkit-fill-available]"
-              />
-            </div>
-            <div className="p-4">
-              <h2 className="text-xl font-bold mb-2">{r.name}</h2>
-              <p className="text-gray-700 mb-2">Food type: {r.cuisine_type}</p>
-              <div className="flex items-center justify-between">
-                <div className="flex items-center gap-2">
-                  <RemoveBtn id={r._id} />
-                  <Link
-                    href={`/edit-restaurant/${r._id}`}
-                    className="text-gray-500 hover:text-gray-700 transition duration-300 ease-in-out"
-                  >
-                    <HiPencilAlt size={24} />
-                  </Link>
-                </div>
-                <LikeBtn restaurantId={r._id} />
+        <div
+          key={r._id}
+          className="border border-gray-200 rounded-lg overflow-hidden bg-white shadow-md transition duration-300 hover:shadow-xl"
+        >
+          <div className="relative w-full h-64">
+            <Image
+              src={r.image ?? "/restaurant-default-image.jpg"}
+              alt={r.name + " Restaurant"}
+              width={1000}
+              height={1000}
+              className="w-fit mx-auto h-[-webkit-fill-available]"
+            />
+          </div>
+          <div className="p-4">
+            <h2 className="text-xl font-bold mb-2">{r.name}</h2>
+            <p className="text-gray-700 mb-2">Food type: {r.cuisine_type}</p>
+            <div className="flex items-center justify-between">
+              <div className="flex items-center gap-2">
+                <RemoveBtn id={r._id} />
+                <Link
+                  href={`/edit-restaurant/${r._id}`}
+                  className="text-gray-500 hover:text-gray-700 transition duration-300 ease-in-out"
+                >
+                  <HiPencilAlt size={24} />
+                </Link>
               </div>
+              <Link
+                key={r._id}
+                href={`/restaurant/${r._id}`}
+                className="bg-slate-700 text-white px-4 py-1 rounded-md transition duration-300 ease-in-out hover:bg-slate-600"
+              >
+                Details
+              </Link>
+              <LikeBtn restaurantId={r._id} />
             </div>
           </div>
-        </Link>
+        </div>
       ))}
     </div>
   );
